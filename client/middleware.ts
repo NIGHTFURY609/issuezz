04d2c8b2423825e00651c1b10f14cf3e894d95ad
NIGHTFURY609 <121104372+NIGHTFURY609@users.noreply.github.com>
2025-05-09 23:59:11 +0530

    fixed some page wrapping in clerk

--- client/middleware.ts
+++ client/middleware.ts
@@ -5,8 +5,9 @@ export default clerkMiddleware();
 export const config = {
   
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Match everything except _next/static, static files, and the home page (/)
+    '/((?!_next|favicon.ico|sign-in|sign-up|^$|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
